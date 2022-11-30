01bd3b98a75cab390faa3b02c512c56a7d2afcac
SongNoin <as5427072@gmail.com>
2022-11-30 22:48:42 +0900

    FEAT 로또 UI 틀만

--- noin-land/src/components/unit/lotto/lotto.Container.tsx
+++ noin-land/src/components/unit/lotto/lotto.Container.tsx
@@ -1,17 +1,58 @@
+import axios from "axios";
+import { colors } from "commons/constants/colors";
 import { ILotto } from "commons/types/lotto";
-import { useEffect, useState } from "react";
+import RoundChip from "components/commons/Chip/roundchip";
+import { H2Text, H3Text, H4Text } from "components/commons/styles/text/text";
+import { useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
 
 export default function LottoContainer() {
   const [lottoData, setLottoData] = useState<ILotto>();
+  const drwNo = 1041;
   useEffect(() => {
     getLottoData();
   }, []);
-  const getLottoData = async () => {
-    await fetch(
-      "https://cors-anywhere.herokuapp.com/https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=861"
-    )
-      .then((res) => res.json())
-      .then((data) => setLottoData(data));
-  };
-  return <>{lottoData?.drwtNo1}</>;
+
+  const getLottoData = useCallback(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${drwNo}`
+      )
+      .then((res) => setLottoData(res.data));
+  }, [drwNo]);
+  return (
+    <Wrap>
+      <LottoNumWrap>
+        <H4Text>
+          {" "}
+          <H3Text color={colors.red5}>{lottoData?.drwNo}회</H3Text> 당첨결과
+        </H4Text>
+        <ChipWrap>
+          <RoundChip text={lottoData?.drwtNo1} />
+          <RoundChip text={lottoData?.drwtNo2} />
+          <RoundChip text={lottoData?.drwtNo3} />
+          <RoundChip text={lottoData?.drwtNo4} />
+          <RoundChip text={lottoData?.drwtNo5} />
+          <RoundChip text={lottoData?.drwtNo6} />
+          +
+          <RoundChip text={lottoData?.bnusNo} />
+        </ChipWrap>
+      </LottoNumWrap>
+    </Wrap>
+  );
 }
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 200px;
+`;
+const LottoNumWrap = styled.div`
+  border: 1px solid #ddd;
+  background: #fff;
+  padding: 60px 60px 90px;
+`;
+const ChipWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
